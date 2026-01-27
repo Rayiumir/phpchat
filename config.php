704f8f4d8a3af035f57e15fcd37e8f308a89af18
Rayiumir <swordart2050@gmail.com>
2026-01-27 18:38:25 +0330

    Create Config.php

---
+++ config.php
@@ -0,0 +1,38 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Access-Control-Allow-Credentials: true");
+
+// Respond if the request is OPTIONS (Preflight)
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+// Setting Database
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'phpchat');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+// Settings Chat
+define('MAX_MESSAGES', 20); // Maximum number of messages
+
+// Connect to Database
+try {
+    $pdo = new PDO(
+        "mysql:host=" . DB_HOST . ";dbname=" . DB_NAME . ";charset=utf8mb4",
+        DB_USER,
+        DB_PASS,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES => false
+        ]
+    );
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
+}
+?>
